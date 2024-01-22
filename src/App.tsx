25af7bc14bd526523621b42104172c13746d1b53
EvertonLDS815 <evertonlino1234@gmail.com>
2024-01-22 13:10:50 -0300

    feat: todo-list configured

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+
+type ItemProps = {
+  id: number;
+  task: string;
+  finished: boolean;
+}
+function App() {
+
+  const [list, setList] = useState<ItemProps[]>([]);
+  const [task, setTask] = useState('');
+
+  function addTask() {
+    const newList = [...list, {
+      id: Math.floor(Math.random() * 10000),
+      task: task,
+      finished: false
+    }];
+
+    setList(newList);
+    setTask('');
+  }
+
+  function handleChangeItem(id: number) {
+    setList((prevState) => {
+      return prevState.map((item) =>
+        item.id === id ? { ...item, finished: !item.finished } : item
+      );
+    });
+  }
+    console.log(list)
+
+  function deleteItem(id: number) {
+    const newList = list.filter((item) => item.id !== id)
+
+    setList(newList)
+  }
+  return (
+    <>
+      <div className="container">
+        <header>
+            <input
+              placeholder="O que tenho que fazer..."
+              className="input"
+              value={task}
+              onChange={(event) => setTask(event.target.value)}
+              />
+            <button
+              className="button"
+              onClick={addTask}
+              >
+                Adicionar
+            </button>
+        </header>
+        <ul className="list-container">
+        {list.length === 0 && (
+          <h2>Não há tarefas</h2>
+        )}
+          {list.length > 0 && (
+            list.map((item: ItemProps) => (
+              <li className={`item ${item.finished && 'active'}`} key={item.id}>
+                  <button
+                    className="edit-button"
+                    onClick={() => handleChangeItem(item.id)}
+                    >
+                      <i className='bx bxs-rocket'></i>
+                  </button>
+                  <span>{item.task}</span>
+                  <button
+                    className="trash-button"
+                    onClick={() => deleteItem(item.id)}>
+                      <i className='bx bxs-trash-alt'></i>
+                  </button>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
